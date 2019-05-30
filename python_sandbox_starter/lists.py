f8d45ac44d79fd1431ed70c06170d4ee837a0263
Rob Campbell <rcampbell1@gal-057049.local>
2019-05-30 12:26:28 -0400

    Finished list practice, moving to tuples_sets

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,41 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create list 
+    #numbers = [1,2,3,4,5]
+fruits=['Apples', 'Oranges', 'Grapes', 'Pears']
+# Use a constructor (NOT USED NORMALLY)
+numbers2 = list((1,2,3,4,5))
+
+# Get Values
+    #print(fruits[2])
+
+# Get Length 
+    #print(len(fruits))
+
+# Append to list 
+fruits.append('Mangoes')
+    #print(fruits)
+
+# Remove from List
+fruits.remove('Grapes')
+
+# Insert into Position
+fruits.insert(2, "Strawberries")
+
+# Change Value
+fruits[0] = 'Blueberries'
+
+# Remove with pop
+fruits.pop(2)
+
+# Reverse the list 
+fruits.reverse()
+
+# Sort list 
+fruits.sort()
+
+# Reverse sort 
+fruits.sort(reverse=True)
+
+print(fruits)
+
