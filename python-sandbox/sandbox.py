d11d0c51e9cbe53bc52fcf7ad54f148807e7aae8
robdcampbell <rdeancampbell@gmail.com>
2021-11-22 16:45:44 -0500

    Contniue Conditional structures in pythonCardio

--- python-sandbox/sandbox.py
+++ python-sandbox/sandbox.py
@@ -2,17 +2,34 @@
 
 
 # Basic Conditional Code:
-
+    # ex 1
             # x = 10
             # if x < 8:
             #     print("Smaller than 8")
             # elif x > 8:
             #     print("Greater than 8")
+    # ex 2
+            # y = 4
 
+            # if y >2:
+            #     print('Bigger')
+            # else:
+            #     print("Smaller")
+            # print("all done")
 
 # Basic While Loop:
-n = 5
-while n > 0:
-    print(n)
-    n= n-1
-print('Blastoff!')
+            # n = 5
+            # while n > 0:
+            #     print(n)
+            #     n= n-1
+            # print('Blastoff!')
+
+# Basic for loop
+
+
+# Check data type
+
+x = "Hello World"
+print(type(x))  # str
+
+
