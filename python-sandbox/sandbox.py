d2f959f2dd6603adaf847a8d51bbaa7227bb599e
robdcampbell <rdeancampbell@gmail.com>
2021-11-22 12:49:38 -0500

    Syntax intro and conditionals + loops

--- python-sandbox/sandbox.py
+++ python-sandbox/sandbox.py
@@ -1,1 +1,18 @@
-print('Hello World')
+# print('Hello World')
+
+
+# Basic Conditional Code:
+
+            # x = 10
+            # if x < 8:
+            #     print("Smaller than 8")
+            # elif x > 8:
+            #     print("Greater than 8")
+
+
+# Basic While Loop:
+n = 5
+while n > 0:
+    print(n)
+    n= n-1
+print('Blastoff!')
