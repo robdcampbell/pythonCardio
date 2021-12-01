c4dfef973f3dbb471d14b785e4e54e1d90de9410
robdcampbell <rdeancampbell@gmail.com>
2021-12-01 15:35:37 -0500

    Python cardio 3/100

--- python-sandbox/dataStructures.py
+++ python-sandbox/dataStructures.py
@@ -6,3 +6,28 @@
 # ///////////////////////////////////////
 #LISTS
 
+# methods: 
+# list()
+# example.sort()
+# len(listEx)
+# max(listEx)
+# min(listEx)
+# sum(listEx)
+
+print(range(4))
+friends = ['Joseph', "Glenn", "Sally"]
+nums = [12,23,44,5,64564565,8,0,-23]
+        # for i in range(len(friends)):
+        #     friend = friends[i]
+        #     print("Happy New Year: ", friend)
+sortedNums = nums.sort()
+print(sortedNums)
+friends.sort()
+print(friends)
+
+import math
+
+print(math.floor(232.443))
+
+# /////////////////////
+#4:06:38
