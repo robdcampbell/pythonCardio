fdd22269f9350ba698efa54ead000c502fdd31e8
robdcampbell <rdeancampbell@gmail.com>
2021-12-02 11:48:49 -0500

    Python Cradio 4/100

--- python-sandbox/dataStructures.py
+++ python-sandbox/dataStructures.py
@@ -9,25 +9,36 @@
 # methods: 
 # list()
 # example.sort()
+# example.append()
 # len(listEx)
 # max(listEx)
 # min(listEx)
 # sum(listEx)
 
-print(range(4))
-friends = ['Joseph', "Glenn", "Sally"]
-nums = [12,23,44,5,64564565,8,0,-23]
-        # for i in range(len(friends)):
-        #     friend = friends[i]
-        #     print("Happy New Year: ", friend)
-sortedNums = nums.sort()
-print(sortedNums)
-friends.sort()
-print(friends)
+      # example = list()
+      # example.append('first')
+      # example.append('second')
+      # example.append('third')
+      # example.insert(0,'zero')
+      # print(example)
 
-import math
 
-print(math.floor(232.443))
+      # print(range(4))
+      # friends = ['Joseph', "Glenn", "Sally"]
+      # nums = [12,23,44,5,64564565,8,0,-23]
+      #         # for i in range(len(friends)):
+      #         #     friend = friends[i]
+      #         #     print("Happy New Year: ", friend)
+      # sortedNums = nums.sort()
+      # print(sortedNums)
+      # friends.sort()
+      # print(friends)
 
-# /////////////////////
-#4:06:38
+      # import math
+
+      # print(math.floor(232.443))
+
+
+# ///////////////////////////////////////
+# DICTIONARIES - Collection of data (similar to an Object in JavaScript)
+
