eb15c233172193235b6de0618aebcb787c364090
robdcampbell <rdeancampbell@gmail.com>
2021-12-02 22:23:12 -0500

    Python Cardio 4.2/100

--- python-sandbox/dataStructures.py
+++ python-sandbox/dataStructures.py
@@ -42,3 +42,44 @@
 # ///////////////////////////////////////
 # DICTIONARIES - Collection of data (similar to an Object in JavaScript)
 
+#Dictionary methods
+# list(dictEx)
+# dictEx.keys()
+# dictEx.values()
+# dictEx.items() - will return a tuple
+
+
+
+
+# Counting in dictionaries (making histograms)
+
+# the clown ran after the car and the car ran into the tent and the tent fell down on the clown and the car
+
+# text = input("Enter Words:")
+# words = text.split()
+# word_counts = dict()
+# for word in words:
+#   word_counts[word ] = word_counts.get(word, 0) +1
+# print(word_counts)
+
+fname = input("Enter File: ")
+if len(fname) < 1: fname = 'clown.txt'
+
+hand = open(fname)
+
+di = dict()
+for line in hand:
+  line = line.rstrip()
+  # print(line)
+  wds = line.split()
+  print(wds)
+  for w in wds:
+    
+    if w in di:
+      di[w] = di[w]+1
+    else:
+      di[w] = 1
+      print("***NEW***")
+    print(w, di[w])
+print(di)
+
