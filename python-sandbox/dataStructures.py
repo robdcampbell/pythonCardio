5015dc9e51853a92ed867fa5bc142632af26f730
robdcampbell <rdeancampbell@gmail.com>
2021-12-02 22:24:25 -0500

    DataStructures, additional notes

--- python-sandbox/dataStructures.py
+++ python-sandbox/dataStructures.py
@@ -72,14 +72,15 @@ for line in hand:
   line = line.rstrip()
   # print(line)
   wds = line.split()
-  print(wds)
+  # print(wds)
   for w in wds:
     
     if w in di:
       di[w] = di[w]+1
     else:
       di[w] = 1
-      print("***NEW***")
+      # print("***NEW***")
     print(w, di[w])
 print(di)
 
+# 5:23:07 - TUPLES
