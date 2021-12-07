d637e49d1edce1615d46d8c2982d96e936406c02
robdcampbell <rdeancampbell@gmail.com>
2021-12-07 07:06:42 -0500

    RegEx notes Python 7.2/100

---
+++ python-sandbox/regEx.py
@@ -0,0 +1,21 @@
+
+# RegEx in Python 
+
+
+'''
+^        - Matches the beginning of a line
+$        - Matches the end of a line
+.        - Matches any character
+\s       - Matches whitespace
+\S       - Matches any non-whitespace character
+*        - Repeats a character 0 or more times
+*?       - Repeats a character 0 or more times (non-greedy)
++        - Repeats a character 1 or more times 
++?       - Repeats a character 1 or more times (non-greedy)
+[aeiou]  - Matches a single character listed in a set
+[^XYZ]   - Matches a single character not listed in a set
+[a-z0-9] - The set of characters can include a range
+)        - indicates where string extraction is to start
+)        - indicates where string extraction is to end
+'''
+
