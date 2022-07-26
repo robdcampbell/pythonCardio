a48872ebafbe85d26d41d612e4cfe3050ca866cb
robdcampbell <rdeancampbell@gmail.com>
2022-07-26 10:20:17 -0400

    Continuing Python syntax prac

---
+++ 2022/basics.py
@@ -0,0 +1,60 @@
+# BASIC CRASH COURSE. print("hello, world.")
+
+### Declaring variables: 
+my_first_variable = 'Python'
+
+### Commenting : you comment in python using the "#" sign
+
+# normal comment 
+
+"""
+    this is a multi-line comment
+"""
+
+
+### casting to other data types:
+a = str(3)    # x will be '3'
+b = int(3)    # y will be 3
+c = float(3)  # z will be 3.0
+    # getting a type:
+# print(type(a))
+# print(type(b))
+
+
+### Unpacking a collection / similar to destructuring in other languages
+
+fruits = ["apple", "banana", "cherry"]
+x,y,z = fruits
+#   print(x)
+#   print(y)
+#   print(z)
+
+########################################################
+### DATATYPES
+
+# TEXT:     str
+# NUMERIC:  int, float, complex 
+# SEQUENCE: list, tuple, range 
+# MAPPING:  dict 
+# SET:      set, frozenset 
+# BOOLEAN:  bool 
+# BINARY:   bytes, bytearray, memoryview 
+# NONE:     NoneType 
+
+# get a data type: type()
+#   print(type("Hello World"))  # will print: <class 'str'>
+
+### NUMBERS: 
+num1 = 1    # int
+num2 = 2.8  # float
+num3 = 1j   # complex
+# print(type(num1)) # <class 'int'>
+# print(type(num2)) # <class 'float'>
+# print(type(num3)) # <class 'complex'>
+
+
+
+
+
+
+
